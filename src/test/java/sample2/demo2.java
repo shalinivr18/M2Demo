efc8f3f753907b8ea90dbe060f1081b81d5a960d
shalini <shalinivr7@gmail.com>
2025-03-28 10:27:17 +0530

    commit practice

---
+++ src/test/java/sample2/demo2.java
@@ -0,0 +1,10 @@
+package sample2;
+
+public class demo2 {
+	public void m1() {
+	System.out.println("changes made in framework system");
+	}
+	public void m2() {
+		System.out.println("2 nd changes made in framework system");
+		}
+}
